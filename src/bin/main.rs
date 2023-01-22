d140ebcb817acf7ef19f9d3d560358dc2cca3ea8
Mike Salmela <salmela.mike@gmail.com>
2023-01-22 17:03:24 +0200

    Use path from argument

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,27 @@
 use rustywav;
+use std::env;
 
 fn main() {
-    let path = String::from("/home/mike/Code/MPKPlayer/resources/test.wav");
-    match rustywav::read_wav(&path) {
-        Ok(wav) => {
-        println!("{}", (wav.header.riff[0] as char));
-        println!("{}", (wav.header.riff[1] as char));
-        println!("{}", (wav.header.riff[2] as char));
-        println!("{}", (wav.header.riff[3] as char));
-        },
-        Err(err) => {
-            println!("Read wav file {} failed", path);
-            println!("Error: {}", err);
+    let args: Vec<String> = env::args().collect();
+    for arg in &args {
+        println!("{}", arg);
+    }
+    if args.len() > 1 {
+        let path = &args[1];
+        match rustywav::read_wav(&path) {
+            Ok(wav) => {
+            println!("{}", (wav.header.riff[0] as char));
+            println!("{}", (wav.header.riff[1] as char));
+            println!("{}", (wav.header.riff[2] as char));
+            println!("{}", (wav.header.riff[3] as char));
+            },
+            Err(err) => {
+                println!("Read wav file {} failed", path);
+                println!("Error: {}", err);
+            }
         }
+    } else {
+        println!("Add wav path as argument!")
     }
 
 }
